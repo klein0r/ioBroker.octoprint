4f9e466fd60271a2e19df38f417b9b9258e929f0
Matthias Kleine <info@haus-automatisierung.com>
2020-05-14 07:33:11 +0200

    Updated words.js

--- admin/words.js
+++ admin/words.js
@@ -3,23 +3,63 @@
 
 systemDictionary = {
     "IP": {
-        "en": "IP",
-        "de": "IP-Adresse"
+        "en": "IP-Address",
+        "de": "IP Adresse",
+        "ru": "Айпи адрес",
+        "pt": "Endereço de IP",
+        "nl": "IP adres",
+        "fr": "Adresse IP",
+        "it": "Indirizzo IP",
+        "es": "Dirección IP",
+        "pl": "Adres IP",
+        "zh-cn": "IP地址"
     },
     "IP of your OctoPrint installation": {
         "en": "IP of your OctoPrint installation",
-        "de": "IP (oder Hostname) der OctoPrint-Installation"
+        "de": "IP Deiner OctoPrint-Installation",
+        "ru": "IP вашей установки OctoPrint",
+        "pt": "IP da sua instalação OctoPrint",
+        "nl": "IP van uw OctoPrint-installatie",
+        "fr": "IP de votre installation OctoPrint",
+        "it": "IP dell'installazione di OctoPrint",
+        "es": "IP de su instalación de OctoPrint",
+        "pl": "IP twojej instalacji OctoPrint",
+        "zh-cn": "OctoPrint安装的IP"
     },
     "Port": {
         "en": "Port",
-        "de": "Port"
+        "de": "Port",
+        "ru": "порт",
+        "pt": "Porta",
+        "nl": "Haven",
+        "fr": "Port",
+        "it": "Porta",
+        "es": "Puerto",
+        "pl": "Port",
+        "zh-cn": "港口"
     },
     "API-Key": {
         "en": "API-Key",
-        "de": "API-Schlüssel"
+        "de": "API-Schlüssel",
+        "ru": "API-ключ",
+        "pt": "Chave API",
+        "nl": "API sleutel",
+        "fr": "Clé API",
+        "it": "API-Key",
+        "es": "Clave API",
+        "pl": "Klucz API",
+        "zh-cn": "API密钥"
     },
     "API-Key - can be found in the settings section of OctoPrint": {
         "en": "API-Key - can be found in the settings section of OctoPrint",
         "de": "API-Schlüssel - diesen Wert kannst Du in den Einstellungen deiner OctoPrint-Installation finden"
+        "ru": "API-ключ - находится в разделе настроек OctoPrint",
+        "pt": "Chave da API - pode ser encontrada na seção de configurações do OctoPrint",
+        "nl": "API-sleutel - is te vinden in het instellingengedeelte van OctoPrint",
+        "fr": "API-Key - se trouve dans la section des paramètres d'OctoPrint",
+        "it": "Chiave API: è disponibile nella sezione delle impostazioni di OctoPrint",
+        "es": "API-Key: se puede encontrar en la sección de configuración de OctoPrint",
+        "pl": "Klucz API - można go znaleźć w sekcji ustawień OctoPrint",
+        "zh-cn": "API密钥-可以在OctoPrint的设置部分中找到"
     }
 };
